80bcc321c92080d1764b4152cb64d6d99e249c34
Santiago <tabordasantiago350@gmail.com>
2024-07-29 00:48:03 -0500

    Primer ejercicio

---
+++ src/Tipo/Datos.java
@@ -0,0 +1,16 @@
+package Tipo;
+
+public class Datos {
+    public static void main(String[] args) {
+        int edad = 25; 
+        double altura = 1.75; 
+        char inicial = 'J'; 
+        boolean esEstudiante = true; 
+        
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Es estudiante: " + esEstudiante);
+    }
+
+}
